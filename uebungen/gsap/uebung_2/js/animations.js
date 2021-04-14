8b5f76f2b7f2edab9ec0fea9b3566c40a9c656e8
uelischmalz <79833481+uelischmalz@users.noreply.github.com>
2021-04-14 11:28:44 +0200

    Initial Commit

---
+++ uebungen/gsap/uebung_2/js/animations.js
@@ -0,0 +1,56 @@
+/* Animation festlegen */
+
+
+/* Alle Steuerungsbuttons in Variablen speichern */
+/*
+let pauseBtn = document.querySelector("#pause");
+let resumeBtn = document.querySelector("#resume");
+let reverseBtn = document.querySelector("#reverse");
+let restartBtn = document.querySelector("#restart");
+let killBtn = document.querySelector("#kill");
+let seekBtn = document.querySelector("#seek");
+let progressBtn = document.querySelector("#progress");
+let timeScaleFastBtn = document.querySelector("#timeScaleFast");
+let timeScaleSlowBtn = document.querySelector("#timeScaleSlow");
+*/
+
+
+/* Für jeden Steuerungsbutton die entsprechende Funktion gemäss Dokumentation hinterlegen */
+/*
+pauseBtn.addEventListener('click', function(){
+
+});
+
+resumeBtn.addEventListener('click', function(){
+
+});
+
+reverseBtn.addEventListener('click', function(){
+
+});
+
+restartBtn.addEventListener('click', function(){
+  animation.restart();
+});
+
+killBtn.addEventListener('click', function(){
+
+});
+
+seekBtn.addEventListener('click', function(){
+
+});
+
+progressBtn.addEventListener('click', function(){
+
+});
+
+timeScaleFastBtn.addEventListener('click', function(){
+
+});
+
+timeScaleSlowBtn.addEventListener('click', function(){
+
+});
+
+*/
---
+++ uebungen/gsap/uebung_3/js/animations.js
@@ -0,0 +1,7 @@
+/* Timeline erstellen */
+
+
+/* Elemente zur Timeline hinzufügen */
+
+
+/* Ab hier genau gleich wie in Übung 2, einfach überall tl anstelle von animation */
